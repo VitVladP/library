8318a23edd377b5f297bdad01b9f7d5767a16ad8
vitaly <vit.vlad.p@gmail.com>
2020-04-28 12:53:37 +0300

    start

---
+++ src/main/java/ru/project/library/controller/MainController.java
@@ -0,0 +1,32 @@
+package ru.project.library.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+
+        return "welcome"; //view
+    }
+
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+                    String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
+}
